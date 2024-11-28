a6730ceb091b25da630e9ff76a1d3dfd9714223d
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-28 15:08:46 +0100

    add serie tv search

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -7,31 +7,36 @@ export default function AppMain({ movies }) {
 	return (
 		<main className='container'>
 			<div className='column'>
-				{movies.map((movie) => (
-					<div key={movie.id} className='col m-3'>
-						<div className='card bg-dark text-white'>
-							<div className='p-3 card-title'>
-								<h3 className='text-danger'>{movie.title || movie.name}</h3>
-							</div>
-							<div className='card-body'>
-								<p>Titolo originale: {movie.original_title || movie.original_name}</p>
-								<p>Voto: {movie.vote_average}</p>
-								<p>
-									Lingua originale:
-									{getFlag(movie.original_language) === 'unknown' ? (
-										` ${movie.original_language} (non trovata nazionalità)`
-									) : (
-										<img
-											className='mx-2 flag-icon'
-											src={`https://flagcdn.com/w20/${getFlag(movie.original_language)}.png`}
-											alt={movie.original_language}
-										/>
-									)}
-								</p>
+				{movies
+					.filter((movie) => movie.media_type !== 'person')
+					.map((movie) => (
+						<div key={movie.id} className='col m-3'>
+							<div className='card bg-dark text-white'>
+								<div className='p-3 card-title'>
+									<h3 className='text-danger'>{movie.title || movie.name}</h3>
+									<p className='text-warning'>
+										{movie.media_type === 'tv' ? 'Serie TV' : movie.media_type === 'movie' ? 'Film' : movie.media_type}
+									</p>
+								</div>
+								<div className='card-body'>
+									<p>Titolo originale: {movie.original_title || movie.original_name}</p>
+									<p>Voto: {movie.vote_average}</p>
+									<p>
+										Lingua originale:
+										{getFlag(movie.original_language) === 'unknown' ? (
+											` ${movie.original_language} (non trovata nazionalità)`
+										) : (
+											<img
+												className='mx-2 flag-icon'
+												src={`https://flagcdn.com/w20/${getFlag(movie.original_language)}.png`}
+												alt={movie.original_language}
+											/>
+										)}
+									</p>
+								</div>
 							</div>
 						</div>
-					</div>
-				))}
+					))}
 			</div>
 		</main>
 	)
