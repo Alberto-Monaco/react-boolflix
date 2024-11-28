b2af0b3d417c85b8be6b945e5b61fdc10c9f5b80
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-28 14:27:53 +0100

    add function flag and db flag

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,17 +1,33 @@
+import languageToFlag from '../db/languageFlags'
 export default function AppMain({ movies }) {
+	function getFlag(languageCode) {
+		return languageToFlag[languageCode] || 'unknown'
+	}
+
 	return (
 		<main className='container'>
-			<div className='column '>
+			<div className='column'>
 				{movies.map((movie) => (
 					<div key={movie.id} className='col m-3'>
 						<div className='card bg-dark text-white'>
-							<div className='p-3 card-title '>
+							<div className='p-3 card-title'>
 								<h3 className='text-danger'>{movie.title}</h3>
 							</div>
 							<div className='card-body'>
 								<p>Titolo originale: {movie.original_title}</p>
 								<p>Voto: {movie.vote_average}</p>
-								<p>Lingua originale: {movie.original_language}</p>
+								<p>
+									Lingua originale:
+									{getFlag(movie.original_language) === 'unknown' ? (
+										` ${movie.original_language} (non trovata nazionalità)`
+									) : (
+										<img
+											className='mx-2 flag-icon'
+											src={`https://flagcdn.com/w20/${getFlag(movie.original_language)}.png`}
+											alt={movie.original_language}
+										/>
+									)}
+								</p>
 							</div>
 						</div>
 					</div>
