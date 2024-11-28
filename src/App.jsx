6da651dafeee775454fff279d12d8e7d78d3e464
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-28 12:07:46 +0100

    add header and form search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import AppHeader from './components/AppHeader'
 import AppMain from './components/AppMain'
 
 function App() {
+	const [searchText, setSearchText] = useState('')
+	const [movies, setMovies] = useState([])
+
 	return (
 		<>
 			<AppHeader />
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,16 @@
 export default function AppHeader() {
 	return (
-		<header>
-			<h1>AppHeader</h1>
+		<header className='bg-dark py-3'>
+			<div className='container d-flex align-items-center justify-content-between'>
+				<h1 className='mb-0 text-danger'>Boolflix</h1>
+
+				<form className='search-bar w-50 d-flex justify-content-end'>
+					<input type='text' className='form-control' placeholder='Cerca un film...' />
+					<button className='btn btn-danger mx-4' type='submit'>
+						Cerca
+					</button>
+				</form>
+			</div>
 		</header>
 	)
 }
