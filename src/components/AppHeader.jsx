c02510ec9d12c43dea7d2ba86232cccc4513935b
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-28 16:27:04 +0100

    create searchbar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,9 +1,5 @@
+import SearchBar from './searchBar'
 export default function AppHeader({ searchText, setSearchText, handleSearch }) {
-	function handleSubmit(e) {
-		e.preventDefault()
-		handleSearch()
-	}
-
 	return (
 		<header className='bg-dark py-3'>
 			<div className='container d-flex align-items-center justify-content-between'>
@@ -11,20 +7,7 @@ export default function AppHeader({ searchText, setSearchText, handleSearch }) {
 					<h1 className='mb-0 text-danger'>Boolflix</h1>
 				</a>
 
-				<form htmlFor='search-text' className='search-bar w-50 d-flex justify-content-end ' onSubmit={handleSubmit}>
-					<input
-						id='search-text'
-						name='search-text'
-						type='text'
-						className='form-control'
-						placeholder='Cerca un film...'
-						value={searchText}
-						onChange={(e) => setSearchText(e.target.value)}
-					/>
-					<button className='btn btn-danger mx-4' type='submit'>
-						Cerca
-					</button>
-				</form>
+				<SearchBar searchText={searchText} setSearchText={setSearchText} handleSearch={handleSearch} />
 			</div>
 		</header>
 	)
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,23 @@
+export default function SearchBar({ searchText, setSearchText, handleSearch }) {
+	function handleSubmit(e) {
+		e.preventDefault()
+		handleSearch()
+	}
+
+	return (
+		<form htmlFor='search-text' className='search-bar w-50 d-flex justify-content-end ' onSubmit={handleSubmit}>
+			<input
+				id='search-text'
+				name='search-text'
+				type='text'
+				className='form-control'
+				placeholder='Cerca un film...'
+				value={searchText}
+				onChange={(e) => setSearchText(e.target.value)}
+			/>
+			<button className='btn btn-danger mx-4' type='submit'>
+				Cerca
+			</button>
+		</form>
+	)
+}
