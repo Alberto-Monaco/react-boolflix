a9b81fbd6b7b36f4b1c765fb658f457d8375c6b8
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-29 19:03:47 +0100

    add search result text

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,10 +2,11 @@ import { useGlobalContext } from '../context/GlobalContext'
 import MovieCard from './MovieCard'
 
 export default function AppMain() {
-	const { movies } = useGlobalContext()
+	const { lastSearch, movies } = useGlobalContext()
 	return (
 		<main className='container'>
-			<div className='row row-cols-1 row-cols-md-2 row-cols-lg-3'>
+			{lastSearch && <h2 className='text-white my-4'>Risultati della ricerca per: "{lastSearch}"</h2>}
+			<div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 '>
 				{movies
 					.filter((movie) => movie.media_type !== 'person')
 					.map((movie) => (
--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -5,10 +5,12 @@ const GlobalContext = createContext()
 function GlobalProvider({ children }) {
 	const [searchText, setSearchText] = useState('')
 	const [movies, setMovies] = useState([])
+	const [lastSearch, setLastSearch] = useState('')
 	const API_URL = `https://api.themoviedb.org/3/search/multi?api_key=7ca648ffa788e60656d044fa86345f4b&query=${searchText}`
 
 	function handleSubmit(e) {
 		e.preventDefault()
+		setLastSearch(searchText)
 
 		fetch(API_URL)
 			.then((response) => response.json())
@@ -23,7 +25,8 @@ function GlobalProvider({ children }) {
 		setSearchText,
 		movies,
 		setMovies,
-		handleSubmit
+		handleSubmit,
+		lastSearch
 	}
 
 	return <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
